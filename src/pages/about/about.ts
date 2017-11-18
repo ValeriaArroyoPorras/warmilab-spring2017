45be3b7c4f441b92224a4f15ee0bcfa955f79a55
Alexander Wong <alwongm@gmail.com>
2017-11-18 12:06:06 -0500

    agregue variables

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -6,7 +6,32 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'about.html'
 })
 export class AboutPage {
-  
+
+  nombre: string = "Alexander Wong";
+  ocupacion: string = "Software Engineer";
+  edad: number = 28;
+
+  datos: any = {
+    nombre: "Alexander Wong",
+    ocupacion: "Software Engineer",
+    edad: 28
+  };
+
+  origen: any = {
+    titulo: "Origen",
+    dato: "Lima, Peru"
+  };
+
+  email: any = {
+    titulo: "E-mail",
+    dato: "alwongm@gmail.com"
+  };
+
+  telefono: any = {
+    titulo: "Telefono",
+    dato: "987531800"
+  };
+
   constructor(public navCtrl: NavController) {
 
   }
